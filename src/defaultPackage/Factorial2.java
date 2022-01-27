e01592b9ef318a90da4bf8f632a1a6b85acf6f93
agrawsou <sourabh.agrawal200.sa@gmail.com>
2022-01-28 00:22:52 +0530

    Commit to Branch X

---
+++ src/defaultPackage/Factorial2.java
@@ -0,0 +1,39 @@
+package defaultPackage;
+
+public class Factorial2 {
+
+	static long fact;
+
+	public static void main(String[] args) {
+
+		// Using while loop
+
+//		while(num!=0) {
+//			fact = fact * num;
+//			num--;
+//		}
+//		
+//		System.out.println("Factorial of the given number is "+fact);
+
+		// Using for loop
+
+//		for(int i=num;i>0;i--) {
+//			fact = fact * i;
+//		}
+//		
+//		System.out.println("Factorial of the given number is "+fact);
+		
+		int factorialValue = recursiveFact(6);
+		
+		System.out.println("Factorial of the given number is "+factorialValue);
+
+	}
+
+	static int recursiveFact(int num) {
+		if(num==0)
+			return 1;
+		else
+		return(num*recursiveFact(num-1));
+	}
+
+}
